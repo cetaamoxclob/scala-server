2a7ab2f7eea8ad3dcf6e6403030b5150aded4f8e
Trevor Allred <trevorallred@gmail.com>
2015-02-18 12:04:24 -0800

    whitespace

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -10,24 +10,24 @@ class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication {
       route(FakeRequest(GET, "/boum")) must beNone
     }
 
-    "render the index page" in new WithApplication{
+    "render the index page" in new WithApplication {
       val home = route(FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("List Tables")
+      contentAsString(home) must contain("List Tables")
     }
 
-    "get data" in new WithApplication{
+    "get data" in new WithApplication {
       val home = route(FakeRequest(GET, "/data/ListTables")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "application/json")
-      contentAsString(home) must contain ("""{"id":"1","data":{"TableDatabase":"1"""")
+      contentAsString(home) must contain( """{"id":"1","data":{"TableDatabase":"1"""")
     }
   }
 }
