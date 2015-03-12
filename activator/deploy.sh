d010cf0d07deb46ca18ff854ad74ff76e7acadb6
Trevor Allred <trevorallred@gmail.com>
2015-03-12 12:47:26 -0700

    cleanup deploy script

--- activator/deploy.sh
+++ activator/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 APP=tantalim/slhp
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" cd ../ && pwd )
+TANTALIM_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )
 
-${DIR}/activator/activator clean docker:stage
-cp Dockerfile target/docker/
-cd target/docker
+cd ${TANTALIM_HOME}/activator/
+echo "Running \"activator clean docker:stage\" in: `pwd`"
+${TANTALIM_HOME}/activator/activator clean docker:stage
+cp ${TANTALIM_HOME}/activator/Dockerfile ${TANTALIM_HOME}/target/docker/
+cd ${TANTALIM_HOME}/target/docker/
+echo "Building docker $APP"
 docker build -t $APP .
 
 # docker push $APP
 
-echo docker run -d -p 9000:9000 -v ${DIR}/tantalim:/opt/docker/tantalim -v ~/slhp/application.conf:/opt/docker/conf/application.prod.conf $APP
+echo docker run -d -p 9000:9000 -v ${TANTALIM_HOME}/tantalim:/opt/docker/tantalim -v ~/slhp/application.conf:/opt/docker/conf/application.prod.conf $APP
