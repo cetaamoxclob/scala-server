d55ba1f729bbd1a88b3c63b2b612c0612d130277
Trevor Allred <trevorallred@gmail.com>
2015-03-12 13:48:27 -0700

    always get the latest before building

--- activator/deploy.sh
+++ activator/deploy.sh
@@ -4,6 +4,7 @@ APP=tantalim/slhp
 TANTALIM_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )
 
 cd ${TANTALIM_HOME}
+git pull
 echo "Running \"activator clean docker:stage\" in: `pwd`"
 ${TANTALIM_HOME}/activator/activator clean docker:stage
 cp ${TANTALIM_HOME}/activator/Dockerfile ${TANTALIM_HOME}/target/docker/
