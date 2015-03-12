747d90f94826a56fb44d9da5c756345a4cbdc665
Trevor Allred <trevorallred@gmail.com>
2015-03-12 15:53:15 -0700

    show logs after restarting

--- activator/restart.sh
+++ activator/restart.sh
@@ -5,3 +5,7 @@ docker rm tantalim
 docker run -d --name tantalim -p 9000:9000 -v \
     /root/slhp/scala-server/tantalim:/opt/docker/tantalim -v \
     /root/slhp/application.conf:/opt/docker/conf/application.conf tantalim/slhp
+sleep 1
+docker logs tantalim
+sleep 5
+docker logs tantalim
