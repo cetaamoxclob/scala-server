6fc93a4b5cad000e402ca903b5dd3ad537c0e1d0
Trevor Allred <trevorallred@gmail.com>
2015-04-13 15:09:43 -0700

    improve help text

--- modules/artifacts/src/main/scala/com/tantalim/artifacts/MissingArtifactException.scala
+++ modules/artifacts/src/main/scala/com/tantalim/artifacts/MissingArtifactException.scala
@@ -4,4 +4,4 @@ import com.tantalim.models.ArtifactType
 import com.tantalim.util.TantalimException
 
 class MissingArtifactException(artifactType: ArtifactType, artifactName: String)
-  extends TantalimException(s"$artifactType ($artifactName) could not be found", "Add the artifact to source")
+  extends TantalimException(s"$artifactType ($artifactName) could not be found", s"Add the artifact to source. <a href='/page/BuildPage/?filter=PageName%20Equals%20%27$artifactName%27'>BuildPage</a>")
