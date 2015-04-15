6cdfcc4dd9ec4dc818092ab8340a2ff64c877a6b
Trevor Allred <trevorallred@gmail.com>
2015-04-15 15:29:19 -0700

    add ModelFieldsEditor and improve sorting

--- modules/artifacts/src/main/scala/com/tantalim/artifacts/MissingArtifactException.scala
+++ modules/artifacts/src/main/scala/com/tantalim/artifacts/MissingArtifactException.scala
@@ -4,4 +4,19 @@ import com.tantalim.models.ArtifactType
 import com.tantalim.util.TantalimException
 
 class MissingArtifactException(artifactType: ArtifactType, artifactName: String)
-  extends TantalimException(s"$artifactType ($artifactName) could not be found", s"Add the artifact to source. <a href='/page/BuildPage/?filter=PageName%20Equals%20%27$artifactName%27'>BuildPage</a>")
+  extends TantalimException(
+    s"$artifactType ($artifactName) could not be found",
+    "Add the artifact to source. " + MissingArtifactException.getLink(artifactType, artifactName)
+  )
+
+object MissingArtifactException {
+  def getLink(artifactType: ArtifactType, artifactName: String) = {
+    val (pageName, fieldName) = artifactType match {
+      case ArtifactType.Page => ("BuildPage", "PageName")
+      case ArtifactType.Menu => ("BuildMenu", "MenuName")
+      case ArtifactType.Model => ("BuildModel", "ModelName")
+      case ArtifactType.Table => ("BuildTable", "TableName")
+    }
+    s"<a href='/page/$pageName/?filter=$fieldName Equals %27$artifactName%27'>$pageName</a>"
+  }
+}
