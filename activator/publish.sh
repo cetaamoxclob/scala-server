9b6195f5b369b1cef49e505381d8897beaaead73
Trevor Allred <trevorallred@gmail.com>
2015-04-29 13:05:08 -0700

    finish first draft of deployment script

--- activator/publish.sh
+++ activator/publish.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
 PROJECT_ROOT=/Users/trevorallred/projects/tantalim/scala
+TARGET_LIB=/Users/trevorallred/projects/tantalim/ide
+
+SBT="./activator"
+
+DIVIDER="-------------------------"
 
 cd ${PROJECT_ROOT}
-sbt clean
-sbt dist
+echo ${DIVIDER}
+echo "Cleaning"
+$SBT clean
+echo ${DIVIDER}
+echo "Packaging"
+$SBT dist
 
-TARGET_LIB=/Users/trevorallred/projects/tantalim/ide/lib
-
-ARTIFACT_VERSION=1.0
-
-modules=(artifacts core database filterCompiler nodes scriptCompiler tantalimModels tantalimServer util)
-
-for i in "${modules[@]}"
-do
-   :
-   ARTIFACT_FILE=${PROJECT_ROOT}/modules/${i}/target/scala-2.11/${i}_2.11-1.0.jar
-   cp ${ARTIFACT_FILE} ${TARGET_LIB}/
-done
-
-#ARTIFACT_ID=tantalim-util
-#ARTIFACT_ID=tantalim-server
-#ARTIFACT_FILE=${PROJECT_ROOT}/target/scala-2.11/tantalim_2.11-1.0.jar
-#
-#mvn install:install-file -DgroupId=com.tantalim -DartifactId=${ARTIFACT_ID} -Dversion=${ARTIFACT_VERSION} \
-#    -Dpackaging=jar -Dfile=${ARTIFACT_FILE} -DlocalRepositoryPath=${REPO_ROOT}
+echo ${DIVIDER}
+echo "Copying Libraries"
+cd ${PROJECT_ROOT}/target/universal/
+rm -Rf tantalim-1.0
+unzip tantalim-1.0.zip
+rm -Rf ${TARGET_LIB}/lib
+cp -R tantalim-1.0/lib ${TARGET_LIB}/
