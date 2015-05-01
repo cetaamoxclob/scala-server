63f8d244db5bea23ba297b2edb58adaf7cab2d6d
Trevor Allred <trevorallred@gmail.com>
2015-05-01 15:09:00 -0700

    better publishing, right side padding and column label

--- activator/publish.sh
+++ activator/publish.sh
@@ -3,22 +3,31 @@
 PROJECT_ROOT=/Users/trevorallred/projects/tantalim/scala
 TARGET_LIB=/Users/trevorallred/projects/tantalim/ide
 
-SBT="./activator"
+SBT="./activator/activator"
 
 DIVIDER="-------------------------"
 
+
 cd ${PROJECT_ROOT}
+echo ${DIVIDER}
+echo "Updating Tantalim Client Libraries"
+public/update_tantalim_client.sh
+
 echo ${DIVIDER}
 echo "Cleaning"
 $SBT clean
+
 echo ${DIVIDER}
 echo "Packaging"
 $SBT dist
 
 echo ${DIVIDER}
-echo "Copying Libraries"
+echo "Unzipping Libraries"
 cd ${PROJECT_ROOT}/target/universal/
 rm -Rf tantalim-1.0
 unzip tantalim-1.0.zip
+
+echo ${DIVIDER}
+echo "Copying Libraries"
 rm -Rf ${TARGET_LIB}/lib
 cp -R tantalim-1.0/lib ${TARGET_LIB}/
